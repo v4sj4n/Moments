148dde5c741892cb401a7a758c4f983651a2730a
Vasjan Çupri <vasjancupri04@gmail.com>
2023-12-23 13:50:55 +0100

    Added 2 pages

---
+++ app/auth/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import { lemonada } from "../layout"
+
+export default function Page() {
+  return (
+    <main className="h-screen w-screen flex flex-col items-center justify-between">
+      <h1 className={`${lemonada.className} -tracking-[4px] text-5xl mt-10`}>
+        <Link href="/">Moments</Link>
+      </h1>
+      <div className="mb-16">
+        <h3 className="text-[40px] font-bold mb-9">Sign in / Sign up</h3>
+        <form className="flex flex-col gap-4">
+          <input
+            className="rounded-md px-4 py-2"
+            type="text"
+            id="identifier"
+            name="identifier"
+            placeholder="enter a username or email"
+          />
+          <button className="bg-slate-200 text-slate-900 rounded-md py-2">
+            continue
+          </button>
+        </form>
+      </div>
+      <p className="mb-10">Enjoy your favorite moments</p>
+    </main>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+import { lemonada } from "./layout"
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen flex flex-col items-center justify-center">
+      <h2 className="text-5xl p-4">Welcome to <span className={`${lemonada.className} -tracking-[4px]`}>Moments</span></h2>
+      <p className="text-xl">save moments, chat and much more together...</p>
+      <Link className="mt-8 px-8 py-2 bg-slate-200 text-slate-900 rounded-md" href="/auth">Get Started</Link>
+    </main>
+  )
+}
