eb7b649626bf6aaad9f6fe96a3a1cfdef9fb9bdd
Vasjan Çupri <vasjancupri04@gmail.com>
2024-02-03 14:59:46 +0100

    Added favicon and made some cosmetic changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,18 @@
 import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className='flex items-center justify-between mx-10  bg-zinc-800 px-12 py-6 rounded-3xl mt-3'>
-      <h2 className='poppins text-4xl font-bold text-center'>
-        Moments<span className='accent-color'>.</span>
-      </h2>
+    <nav className='flex items-center justify-between mx-10  px-9 py-3 mt-3'>
+      <Link href='/dashboard'>
+        <Image
+          src={'/momentsLetterLogo.png'}
+          alt="Moment's letter logo"
+          height={75}
+          width={75}
+        />
+      </Link>
       <UserButton showName />
     </nav>
   )
